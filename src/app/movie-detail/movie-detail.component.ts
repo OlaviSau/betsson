cfbf6edbd9bb9ce0f3bea260e55e4581ba570729
Olavi Sau <olavisau@gmail.com>
2018-10-29 16:08:45 +0200

    Add movie detail skeleton

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AppState} from '../app.state';
+import {Store} from '@ngrx/store';
+import {Movie} from '../models/movie';
+import {ActivatedRoute} from '@angular/router';
+
+interface MovieKeyRoute {
+  key: string;
+}
 
 @Component({
   selector: 'bg-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss']
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  movie: Movie;
+  private routeSubscriber: any;
+
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.routeSubscriber = this.route.params.subscribe(params => {
+        this.store.select('movie').subscribe(
+            movies => this.movie = movies.find(movie => movie.key === params['key'])
+        );
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSubscriber.unsubscribe();
   }
 
 }
